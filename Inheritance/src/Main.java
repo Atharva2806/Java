87bc117acd86999241f2dd2b1dd880ad4efb5dba
Atharva2806 <88070746+Atharva2806@users.noreply.github.com>
2024-03-11 13:38:11 +0530

    Delete Inheritance directory

--- Inheritance/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        Person p1=new Person();
-    }
-}
--- Inheritance/src/Person.java
+++
@@ -1,9 +0,0 @@
-import java.sql.SQLOutput;
-
-public class Person
-{
-    private String name;
-    private int age;
-    public Person()
-\\
-}
